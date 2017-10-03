fd3bd4f2e4cab9b898b8419db6ff870ab483cb65
EC2 Default User <xiasiliang.hit@gmail.com>
2017-10-03 14:12:30 +0000

    userid

---
+++ app/controllers/Identity.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+import models.*;
+
+import java.util.*;
+
+import play.*;
+import play.api.*;
+import play.data.*;
+
+public class Identity extends Controller {
+
+}
+	
