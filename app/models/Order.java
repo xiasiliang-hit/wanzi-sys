5d0d91a6ab3723a5702af708c52a6fd78d788d6f
xiasilaing-hit <xiasilaing.hit@gmail.com>
2017-10-26 14:40:45 +0800

    order comment

--- app/models/Order.java
+++ app/models/Order.java
@@ -9,33 +9,35 @@ import play.modules.mongodb.jackson.MongoDB;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Entity
-//@Table(name="order")
+//游客下单购买导游服务
 public class Order {
-	public Order()
-	{}
+    public Order()
+    {}
 
-	@Id
-	@ObjectId
-	public String id;
-	public String buyer_id;
-	public String buyer_name;
-	public String seller_id;
-	public String seller_name;
+    @Id
+    @ObjectId
+    public String id;
+    public String traveller_id;  //游客id
+    public String traveller_name;
+    public String guider_id;  //
+    public String guider_name;
 	
-	//	public HashMap serviceitems<String, int>; // (String service_name, int per_pirce, int quote, int item_price) // service_name=(GUIDER, CAR, PIACKUP)
-	public int total_price;
-	public String payment_timestamp; //DateTime
-	public String status;
-	public String comment;
-	public String policy;
+    //	public HashMap serviceitems<String, int>; // (String service_name, int per_pirce, int quote, int item_price) // service_name=(GUIDER, CAR, PIACKUP)
+                                                  // 服务名称（导游，带车导游，接送机），单价，服务数量（天数），小记＝单价＊天数
+    public int total_price;  //服务总价
+    public String payment_timestamp; //DateTime
+    public String status;  //订单状态
+    public String comment;  //暂时空置
+    public String policy;  //页面上的三种取消政策
 
+
+    /* 订单状态 */
 	public static String UNPAID = "UNPAID";
 	public static String PAID = "PAID";
 	public static String INSERVICE = "INSERVICE";
 	public static String COMPLETE= "COMPLETE";
 	public static String USERCONFIRMED= "USERCONFIRMED";
-
+    /* 取消政策 */
 	public static String POLICY_STRICT = "STRICT";
 	public static String POLICY_MIDIUM = "MIDIUM";
 	public static String POLICY_LOOSE = "LOOSE";
